06735a9cea86353c747ab89b80664fbbfee68bc6
ojasgupta-25 <ojasgupta25@gmail.com>
2023-06-20 16:50:55 -0500

    added project

---
+++ src/test/java/com/ojas/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ojas.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
